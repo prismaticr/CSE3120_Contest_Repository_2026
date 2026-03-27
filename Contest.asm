09e5250b7b8c9997cda3403d0922282b8476e185
prismaticr <166749793+prismaticr@users.noreply.github.com>
2026-03-27 19:38:52 -0400

    Upload game foundation

--- Contest.asm
+++ Contest.asm
@@ -1,8 +1,14 @@
+INCLUDE Irvine32.inc
 .386
-.model flat,stdcall
 .stack 4096
 ExitProcess PROTO, dwExitCode:DWORD
 .data
+balance DWORD 100 ; how much the player starts with
+
+; message to explain game and rules
+greet BYTE "Welcome to the Casino! Where money is pratically free! ", 0Dh, 0Ah
+   BYTE "Type g to gamble or l to leave if you're a coward! "
+   BYTE "You're current balance is: ", 0
 
 .code
 main PROC
